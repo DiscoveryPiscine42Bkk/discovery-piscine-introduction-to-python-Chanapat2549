e47763896ba447681b1b29b08bb5f07299e94abe
Chanapat2549 <68030054@kmitl.ac.th>
2025-06-16 09:08:29 +0000

    42.py

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Chanapat"
+last_name = "Nakornkasem"
+print(first_name+" "+last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Chanapat"
+last_name = "Nakornkasem"
+whole_name = first_name+" "+last_name
+print(whole_name)
